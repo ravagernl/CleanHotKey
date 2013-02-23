bccf4165d53940f221f3484e397b7b0da39bc90c
Ruben Vincenten <rubenvincenten@gmail.com>
2013-02-23 20:32:10 +0100

    fail.

--- addon.lua
+++ addon.lua
@@ -1,4 +1,4 @@
 local nfns, font, size = NumberFontNormalSmall
-path, size = NumberFontNormalSmall:GetFont()
+font, size = NumberFontNormalSmall:GetFont()
 nfns:SetFont(font, size, 'OUTLINE')
 nfns:SetShadowColor(0, 0, 0, 0)
